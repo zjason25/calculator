df825fd341d830b532973980915b4bdbbe1d8b83
zjason25 <90489418+zjason25@users.noreply.github.com>
2024-06-14 16:52:28 -0700

    Add UpdateDisplay() and enables basic operations

--- functions.js
+++ functions.js
@@ -7,9 +7,20 @@ buttons.forEach((button) => {
     const btnClass = button.getAttribute("class");
     const btnValue = button.getAttribute("value");
     updateNums(btnClass, btnValue);
+    updateDisplay(btnClass);
   });
 });
 
+function updateDisplay(btnClass) {
+  if (btnClass == "number" || btnClass == "clear" || btnClass == "equal") {
+    if (op == "") {
+      display.textContent = num1;
+    }
+    else {
+      display.textContent = num2;
+    }
+  }
+}
 function updateNums(btnClass, btnValue) {
   if (btnClass == "clear") {
     num1 = 0, num2 = 0, op = "";
@@ -33,12 +44,17 @@ function updateNums(btnClass, btnValue) {
   else if (btnClass == "operator") {
     op = btnValue;
   }
+  else if (btnClass == "equal") {
+    num1 = operate(num1, num2, op);
+    op = "";
+    num2 = 0;
+  }
   console.log(num1 + " " + op + " " + num2);
 }
 
 function operate(num1, num2, op) {
   if (op == "+") {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
   }
   else if (op == "-") {
     return num1 - num2;
